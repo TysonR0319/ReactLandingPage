2be27f5f2dec8f28008b61e9e560147b61a5790c
TysonR0319 <tysonr0319@gmail.com>
2022-07-08 01:10:40 +0000

    fixed style import

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import css from "/workspace/react-hello/src/styles/index.css"
+
 export const Footer = () => {
     return (
         <div className="footer container-fluid justify-content-center align-items-center h-5 bg-dark d-flex" >
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,7 @@ import {Navbar} from "./navbar.jsx"
 import { Jumbotron } from "./jumbotron.jsx";
 import { Cards } from "./cards.jsx";
 import { Footer } from "./footer.jsx";
-import css from "/workspace/react-hello/src/styles/index.css"
+
 //create your first component
 const Home = () => {
 	return (
